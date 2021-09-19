5c5185eb8089a56c15f16ce54e2f0c4bdeb6083e
tony <tonychunenlin@gmail.com>
2021-09-19 12:17:17 +0800

    init

---
+++ bio.py
@@ -0,0 +1,18 @@
+
+def hello():
+    print("Hi :), This is Chun En Lin !")
+
+def info():
+    print("I'm currently working as a Mackine Learning engineer.\n")
+    print("im specialized and interested with ML/DP, data analyst.\n")
+    print("Moreover, dedicated to become full-stack Data scientist!")
+    
+def skills():
+    print("My relevant skills are EDA, modelling and data visulization.\n")
+    print("Build all sort of ETL and serving pipeline for end to end service.\n")
+    print("Utilizing usefull tools to process big data such as pyspark!")
+
+
+
+
+
